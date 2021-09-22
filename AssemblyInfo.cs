07845716618a9191a0246ea8dc5b86863604404c
心倾 <38943319+LHTXQ@users.noreply.github.com>
2021-09-22 16:56:52 +0800

    1.1.1.1

---
+++ AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("To-do List")]
+[assembly: AssemblyDescription("待办清单")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("心倾")]
+[assembly: AssemblyProduct("To-do List")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("心倾")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("53fafd6b-c67a-449c-879f-33ce8c9c663a")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
+//通过使用 "*"，如下所示:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1.1")]
+[assembly: NeutralResourcesLanguage("zh-Hans")]
